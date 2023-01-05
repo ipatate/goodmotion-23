62aa94fa0905c2bfe1e9be28a22846e576a83e05
ipatate <faramazpat@gmail.com>
2023-01-05 15:36:40 +0100

    fix style frame

---
+++ patterns/frame-bg.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: Frame BG
+ * Slug: goodmotion/frame-bg
+ * Categories: goodmotion/goodmotion-patterns
+ */
+
+?>
+
+<!-- wp:group {"backgroundColor":"gray-clear","className":"gm-frame-bg","layout":{"type":"constrained"}} -->
+<div class="wp-block-group gm-frame-bg has-gray-clear-background-color has-background"><!-- wp:paragraph -->
+  <p>...</p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
---
+++ patterns/frame-border.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Title: Frame Border
+ * Slug: goodmotion/frame-border
+ * Categories: goodmotion/goodmotion-patterns
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","className":"gm-frame","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide gm-frame">
+  <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"text","fontSize":"2xlarge"} -->
+  <p class="has-text-color has-2-xlarge-font-size" style="font-style:normal;font-weight:600">....</p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
